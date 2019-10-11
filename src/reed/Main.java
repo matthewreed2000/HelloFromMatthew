284eb6edb12092ad31c70156272f82da7ee112ca
Matthew A Reed <matthewreed2000@byui.edu>
2019-10-10 18:23:08 -0600

    Added ya BOY

--- src/reed/Main.java
+++ src/reed/Main.java
@@ -3,6 +3,6 @@ package reed;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Matthew and " +
-                "Ryan is the best");
+                "Ryan is the best" + "Ya BOY!");
     }
 }
